40bbbcd885f90720fc58c7cb9f245b2de38b2c81
IrynaMasechko94 <165693960+IrynaMasechko94@users.noreply.github.com>
2024-08-28 20:16:49 +0300

    second commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.body;
 
+let colorInterval;
+
+startButton.addEventListener('click', () => {
+  colorInterval = setInterval(changeColor, 1000);
+  startButton.disabled = true;
+  stopButton.disabled = false;
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(colorInterval);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+});
+
+function changeColor() {
+  let randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
