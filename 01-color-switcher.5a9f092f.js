50ff5f852cf1b89b544f41de8e934083900b98ea
IrynaMasechko94 <165693960+IrynaMasechko94@users.noreply.github.com>
2024-08-28 17:17:39 +0000

    Deploying to gh-pages from @ IrynaMasechko94/goit-js-hw-09@40bbbcd885f90720fc58c7cb9f245b2de38b2c81 🚀

---
+++ 01-color-switcher.5a9f092f.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),d=document.body;let a;function l(){let t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;d.style.backgroundColor=t}t.addEventListener("click",(()=>{a=setInterval(l,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(()=>{clearInterval(a),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.5a9f092f.js.map
